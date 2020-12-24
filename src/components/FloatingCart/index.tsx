422c4bd8076c9e9c3e16420e2cdab6ed747c4f14
gustavoInventdigital <Gustavo@inventdigital.com.br>
2020-12-24 10:00:55 -0300

    finish'

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulater, product) => {
+      const productsSubTotal = product.price * product.quantity;
+      return accumulater + productsSubTotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulater, product) => {
+      const productsQuantity = product.quantity;
+      return accumulater + productsQuantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
